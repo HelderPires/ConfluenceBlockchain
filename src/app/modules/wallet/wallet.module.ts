6e0849f36a7d3675fcfe765d2280a0b72317791a
Helder <h.t.pires84@gmail.com>
2021-09-23 15:56:56 +0200

    added feature component scaffolds

--- src/app/modules/wallet/wallet.module.ts
+++ src/app/modules/wallet/wallet.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WalletComponent } from './wallet.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WalletComponent
+  ],
   imports: [
     CommonModule
   ]
